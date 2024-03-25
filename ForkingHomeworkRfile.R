a01331dcef7b3f459043deb6d1a91a4f4579845c
Miles Matthews <151972422+MilesMatthews@users.noreply.github.com>
2024-03-25 19:42:42 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,5 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+table_new2 <- diamonds[, list(cut, price, carat)][cut == "Premium" & price > 7000 & price < 10000][order(-carat)][1:20]
